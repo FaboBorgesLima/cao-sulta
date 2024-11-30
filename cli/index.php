684f60a919d3b5c2056e02bc7bdf4b94dca2c672
FaboBorgesLima <fabo.borges.lima@gmail.com>
2024-11-30 00:46:55 -0300

    chore: adding some cli commands and dirs

---
+++ cli/index.php
@@ -0,0 +1,93 @@
+<?php
+
+require __DIR__ . "/../vendor/autoload.php";
+
+use Lib\StringUtils;
+
+/**
+ * @param array<string,string> $args
+ */
+function create($args)
+{
+    if (!array_key_exists(0, $args) || !array_key_exists(1, $args)) {
+        echo "-------------- commands --------------
+    create controller <NameController>
+    create model <Name>
+";
+        return;
+    }
+
+    $object = $args[0];
+    $name = $args[1];
+
+    if (!StringUtils::isUpperCamelCase($name)) {
+        throw new Error("Names must be UpperCamelCase");
+    }
+
+
+    switch ($object) {
+        case "controller":
+            if (!str_ends_with($name, "Controller")) {
+                echo "-------------- commands --------------
+    create controller <NameController>
+    create model <Name>
+";
+                return;
+            }
+            $file = fopen(__DIR__ . "/../app/Controller/$name.php", "w");
+            fwrite($file, "<?php
+
+namespace App\Controller;
+
+use Core\Http\Controllers\Controller;
+use Core\Http\Response;
+
+class $name extends Controller
+{
+    public function show(): void
+    {
+        echo \"$name\";
+    }
+}
+");
+            echo "created $name Controller at app/Controller/$name.php\n";
+            break;
+        case "model":
+            $file = fopen(__DIR__ . "/../app/Models/$name.php", "w");
+            $table_name = StringUtils::camelToSnakeCase($name) . "s";
+            fwrite($file, "<?php
+
+namespace App\Models;
+
+use Core\Database\ActiveRecord\Model;
+
+class $name extends Model
+{
+    static protected string \$table = \"$table_name\";
+    protected static array \$columns = [];
+
+    public function validates(): void
+    {
+        \$this->addError(\"implementation\", \"must implement\");
+    }
+}
+");
+            echo "created $name Model at app/Models/$name.php\n";
+            break;
+        default:
+            echo "-------------- commands --------------
+    create controller <NameController>
+    create model <Name>
+";
+    }
+}
+
+
+if (array_key_exists(1, $argv) && function_exists($argv[1])) {
+    $args = array_splice($argv, 2);
+    call_user_func($argv[1], $args);
+} else {
+    echo "-------------- commands --------------
+    create controller <NameController>
+    create model <Name>";
+}
