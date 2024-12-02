e588f5aea9f903f856a8330c8893ef43b078284e
FaboBorgesLima <fabo.borges.lima@gmail.com>
2024-12-01 21:55:02 -0300

    chore: renaming folder

---
+++ Lib/StringUtils.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Lib;
+
+class StringUtils
+{
+    public static function camelToSnakeCase($string)
+    {
+        return strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $string));
+    }
+
+    public static function lowerSnakeToCamelCase($string)
+    {
+        $camel_case = preg_replace_callback(
+            '/_([a-zA-Z])/',
+            fn($match) => strtoupper($match[1]),
+            $string
+        );
+
+        return $camel_case;
+    }
+
+    public static function isNumeric(string $str): bool
+    {
+        return ! (bool) preg_match('/[^0-9]/', $str);
+    }
+
+    public static function snakeToCamelCase($string)
+    {
+        return ucfirst(self::lowerSnakeToCamelCase($string));
+    }
+
+    public static function isUpperCamelCase($string): bool
+    {
+        preg_match('/^([A-Z][a-z0-9]+)+/', $string, $matches);
+
+        if (count($matches) == 0) {
+            return false;
+        }
+
+        return $matches[0] == $string;
+    }
+}
