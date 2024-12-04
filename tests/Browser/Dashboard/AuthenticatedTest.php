e11a76ca7303bfaa2bae01713e4dacaed98c177f
FaboBorgesLima <fabo.borges.lima@gmail.com>
2024-12-03 21:31:16 -0300

    test: adding ControllerTestCase, TestCase and AuthenticatedTest

---
+++ tests/Browser/Dashboard/AuthenticatedTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Browser\Problems;
+
+use PHPUnit\Framework\TestCase as FrameworkTestCase;
+
+class AuthenticatedTest extends FrameworkTestCase
+{
+    public function test_should_redirect_if_not_authenticated_to_auth(): void
+    {
+        $page = file_get_contents('http://web/dashboard');
+
+        $statusCode = $http_response_header[0];
+        $location = $http_response_header[10];
+
+        $this->assertEquals('HTTP/1.1 302 Found', $statusCode);
+        $this->assertEquals('Location: /auth', $location);
+    }
+}
