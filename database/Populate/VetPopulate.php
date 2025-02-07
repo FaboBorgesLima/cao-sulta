04f7150fdb43c6a77e080e0d75d2925534329fe2
FaboBorgesLima <fabo.borges.lima@gmail.com>
2025-02-07 04:59:57 -0300

    fix: fixing populate

--- database/Populate/VetPopulate.php
+++ database/Populate/VetPopulate.php
@@ -17,7 +17,7 @@ class VetPopulate
             /** @var \App\Models\Vet */
             $vet = $users[$i * 2]->vet()->new([]);
 
-            $vet->attachCRMVRegister(new CRMVRegister(["crmv" => "202200$i", "state" => Random::state()]));
+            $vet->attachCRMVRegister(CRMVRegister::make(["crmv" => "202200$i", "state" => Random::state()]));
 
             $vet->save();
         }
